0ed52b9151e54542e135a7f86ba7996195fb3de3
aifromgyumri <aifromgyumri@gmail.com>
2020-05-14 21:57:45 +0400

    First commit**

---
+++ QBSIssueTracker/QBSIssueTracker.Android/obj/Debug/90/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: c8a6d3c053a857f2b5c8b11e0a1f4c0a819fabad */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	7240
+	/* entry-length */
+	.word	266
+	/* value-offset */
+	.word	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 1925841
+	.include	"typemap.mj.inc"
---
+++ QBSIssueTracker/QBSIssueTracker.Android/obj/Debug/90/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: c8a6d3c053a857f2b5c8b11e0a1f4c0a819fabad */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	7240
+	/* entry-length */
+	.long	266
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 1925841
+	.include	"typemap.mj.inc"
